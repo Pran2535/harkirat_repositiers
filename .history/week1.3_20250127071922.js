37d692e237d4e699dd3ffb77335e39f255101f15
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-01-27 07:25:13 +0530

    harkirat singh starters

---
+++ .history/week1.3_20250127071922.js
@@ -0,0 +1,6 @@
+function getlength(str) {
+  console.log("original string", str);
+  console.log("length", str.length);
+}
+
+getlength("Hello World!");
---
+++ .history/week1.3_20250127072155.js
@@ -0,0 +1,14 @@
+function getlength(str) {
+  console.log("original string", str);
+  console.log("length", str.length); // str gives us property to get length of the function
+}
+
+getlength("Hello World!");
+
+// finding index of the function
+function findindexof(str, target) {
+  console.log("original string", str);
+  console.log("Index", str.indexof(target));
+}
+
+findindexof("Hello World!", "world");
